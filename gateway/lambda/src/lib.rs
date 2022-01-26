6bb336139a81d603bb38048cd579e2bec28cd348
medelman17 <mike.edelman@gmail.com>
2022-01-26 05:49:55 -0500

    feat: spin up reviews service

---
+++ gateway/lambda/src/lib.rs
@@ -0,0 +1,9 @@
+use tracing_subscriber;
+
+pub fn init_tracing() {
+    let subscriber = tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .json()
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("failed to set tracing subscriber");
+}
--- services/reviews/bin/reviews.ts
+++ services/reviews/bin/reviews.ts
@@ -4,6 +4,9 @@ import * as cdk from 'aws-cdk-lib';
 import { ReviewsStack } from '../lib/reviews-stack';
 
 const app = new cdk.App();
-new ReviewsStack(app, 'CustomersStack', {
-
+new ReviewsStack(app, 'ReviewsStack', {
+  env: {
+    account: '661099804654',
+    region: 'us-west-2',
+  },
 });
